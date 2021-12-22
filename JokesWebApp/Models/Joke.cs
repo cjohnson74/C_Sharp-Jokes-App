0bd636d49f27029a6adc8351c3a835abe64f764c
Carson Johnson <carson74johnson@gmail.com>
2021-12-21 23:23:26 -0500

    Added constructor to Joke models

--- JokesWebApp/Models/Joke.cs
+++ JokesWebApp/Models/Joke.cs
@@ -6,6 +6,10 @@ namespace JokesWebApp.Models
         public int Id { get; set; }
         public string JokeQuestion { get; set; }
         public string JokeAnswer { get; set; }
-        
+
+        public Joke()
+        {
+
+        }
     }
 }
