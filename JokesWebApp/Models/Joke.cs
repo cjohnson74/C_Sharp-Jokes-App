510db53e5746276562ebe93dbca8808af2be29f2
Carson Johnson <carson74johnson@gmail.com>
2021-12-21 23:21:28 -0500

    Added properties to the Joke model

--- JokesWebApp/Models/Joke.cs
+++ JokesWebApp/Models/Joke.cs
@@ -2,9 +2,10 @@
 namespace JokesWebApp.Models
 {
     public class Joke
-    {
-        public Joke()
-        {
-        }
+    { 
+        public int Id { get; set; }
+        public string JokeQuestion { get; set; }
+        public string JokeAnswer { get; set; }
+        
     }
 }
