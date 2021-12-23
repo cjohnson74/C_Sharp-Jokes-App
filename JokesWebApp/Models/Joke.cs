66bf25a52f5e98444ba5631e271dae76e10e9d0d
Carson Johnson <carson74johnson@gmail.com>
2021-12-22 22:36:04 -0500

    Created new Joke class

--- JokesWebApp/Models/Joke.cs
+++ JokesWebApp/Models/Joke.cs
@@ -2,14 +2,9 @@
 namespace JokesWebApp.Models
 {
     public class Joke
-    { 
-        public int Id { get; set; }
-        public string JokeQuestion { get; set; }
-        public string JokeAnswer { get; set; }
-
+    {
         public Joke()
         {
-
         }
     }
 }
